af5a45f32597dc86e91625e274b199a9aa8d2618
tsaohucn <tsaohucn@gmail.com>
2018-04-20 01:10:51 +0700

    add signUpOne

---
+++ app/src/main/java/com/example/faketsao/dating/SignUpOne.java
@@ -0,0 +1,13 @@
+package com.example.faketsao.dating;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_one);
+    }
+}
