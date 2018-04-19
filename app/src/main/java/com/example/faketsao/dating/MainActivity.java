77639a1c8f2c341bff315a78fea64f23e6b0296f
tsaohucn <tsaohucn@gmail.com>
2018-04-20 00:05:32 +0700

    add firebase function

--- app/src/main/java/com/example/faketsao/dating/MainActivity.java
+++ app/src/main/java/com/example/faketsao/dating/MainActivity.java
@@ -2,12 +2,41 @@ package com.example.faketsao.dating;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
+import android.support.annotation.NonNull;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        auth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser() != null) {
+                    // 登入
+                } else {
+                    // 歡迎頁面
+                    setContentView(R.layout.activity_welcome);
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        auth.removeAuthStateListener(authStateListener);
     }
 }
